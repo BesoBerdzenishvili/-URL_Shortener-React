fa2a0d25d416cc90608e7baee4eeb5acc896a8fa
fliderman01 <fliderman01@gmail.com>
2023-02-25 12:23:06 +0400

    added input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import React, { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [inputData, setInputData] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send data to server using fetch
+    fetch("https://vast-lime-walkingstick-wear.cyclic.app/urls", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ longUrl: inputData }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    // Clear input field
+    setInputData("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputData}
+          onChange={(e) => setInputData(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
